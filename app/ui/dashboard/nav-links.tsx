beb2ea88e9308485fc5ad518a1fb2adf4e08b6c2
AlexAirCG <alexairggg@yandex.ru>
2025-10-15 12:20:09 +0300

    15.10.:12:19: improve frontend

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,46 +1,54 @@
 "use client";
-import {
-  CalendarDateRangeIcon,
-  ClipboardDocumentListIcon,
-  ShoppingCartIcon,
-} from "@heroicons/react/24/outline";
+
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FaShopify } from "react-icons/fa6";
+import { LuListTodo } from "react-icons/lu";
+import { IoCalendarNumberOutline } from "react-icons/io5";
 
 // Карта ссылок для отображения в боковой навигации.
-const links = [
-  { name: "Календарь", href: "/dashboard", icon: CalendarDateRangeIcon },
-  {
-    name: "Дела",
-    href: "/dashboard/todo",
-    icon: ClipboardDocumentListIcon,
-  },
-  { name: "Покупки", href: "/dashboard/shop", icon: ShoppingCartIcon },
-];
 
 export default function NavLinks() {
   const pathName = usePathname(); // текущий путь пользователя из URL.
   return (
     <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-200 p-3 text-[16px] font-medium hover:bg-sky-200 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              {
-                "bg-sky-200 text-blue-600": pathName === link.href,
-              }
-            )}
-          >
-            <LinkIcon className="w-7" />
-            <p className="hidden md:block">{link.name}</p>
-          </Link>
-        );
-      })}
+      <Link
+        className={clsx(
+          "flex grow items-center justify-center gap-2 rounded-md bg-gray-200 p-2 text-[16px] font-medium hover:bg-sky-200 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+          {
+            "bg-sky-200 text-blue-600": pathName === "/dashboard",
+          }
+        )}
+        href={"/dashboard"}
+      >
+        <IoCalendarNumberOutline className="w-6 h-6" />
+        Календарь
+      </Link>
+      <Link
+        className={clsx(
+          "flex  grow items-center justify-center gap-2 rounded-md bg-gray-200 p-2 text-[16px] font-medium hover:bg-sky-200 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+          {
+            "bg-sky-200 text-blue-600": pathName === "/dashboard/todo",
+          }
+        )}
+        href={"/dashboard/todo"}
+      >
+        <LuListTodo className="w-6 h-6" />
+        Дела
+      </Link>
+      <Link
+        className={clsx(
+          "flex grow items-center justify-center gap-2 rounded-md bg-gray-200 p-2 text-[16px] font-medium hover:bg-sky-200 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+          {
+            "bg-sky-200 text-blue-600": pathName === "/dashboard/shop",
+          }
+        )}
+        href={"/dashboard/shop"}
+      >
+        <FaShopify className="w-6 h-6" />
+        Покупки
+      </Link>
     </>
   );
 }
