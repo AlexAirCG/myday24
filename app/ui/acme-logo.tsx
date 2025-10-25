8a59e074a29ca6de743b96d4c4de4e906bc8ff0d
AlexAirCG <alexairggg@yandex.ru>
2025-10-25 16:01:20 +0300

    25.10.:15:57 - chenged dising

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,9 +3,9 @@ import { lusitana } from "./fonts";
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white `}
+      className={`${lusitana.className} flex items-center justify-center leading-none text-white `}
     >
-      <p className="text-[30px] md:text-[44px] ml-2 text-shadow-lg/30 00 hover:text-shadow-lg/50 00">
+      <p className="text-[30px] md:text-[44px] ml-2 text-shadow-lg/40 hover:text-shadow-lg/50 00">
         myDay24
       </p>
     </div>
