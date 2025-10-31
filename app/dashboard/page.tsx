28ca57a97bb1b093a516f57ea4f7cd05360de913
AlexAirCG <alexairggg@yandex.ru>
2025-10-31 22:00:47 +0300

    31.10.:22:00 - add google auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import { CiBatteryEmpty, CiBatteryFull } from "react-icons/ci";
+import { TbAccessPoint } from "react-icons/tb";
+import { TbAccessPointOff } from "react-icons/tb";
+
 export default function Page() {
-  return <p>Calendar</p>;
+  const [showPass, setShowPass] = useState(true);
+  const [showPower, setShowPower] = useState(true);
+  return (
+    <div>
+      <button type="button" onClick={() => setShowPass(!showPass)}>
+        {showPass ? (
+          <TbAccessPoint className="w-10 h-10" />
+        ) : (
+          <TbAccessPointOff className="w-10 h-10" />
+        )}
+      </button>
+
+      <button type="button" onClick={() => setShowPower(!showPower)}>
+        {showPower ? (
+          <CiBatteryEmpty className="w-10 h-10" />
+        ) : (
+          <CiBatteryFull className="w-10 h-10" />
+        )}
+      </button>
+    </div>
+  );
 }
--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
 import { z } from "zod";
 import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcryptjs";
@@ -18,27 +19,76 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
 
-export const { auth, signIn, signOut } = NextAuth({
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth({
   ...authConfig,
+
   providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      // Если хотите разрешить вход через Google пользователям,
+      // зарегистрированным ранее по email/паролю с тем же email:
+      allowDangerousEmailAccountLinking: true,
+    }),
+    // Credentials({
+    //   async authorize(credentials) {
+    //     const parsedCredentials = z
+    //       .object({ email: z.email(), password: z.string().min(6) })
+    //       .safeParse(credentials);
+
+    //     if (parsedCredentials.success) {
+    //       const { email, password } = parsedCredentials.data;
+    //       const user = await getUser(email);
+    //       if (!user) return null;
+    //       const passwordsMatch = await bcrypt.compare(password, user.password);
+
+    //       if (passwordsMatch) return user;
+    //     }
+
+    //     console.log("Invalid credentials");
+    //     return null;
+    //   },
+    // }),
     Credentials({
       async authorize(credentials) {
-        const parsedCredentials = z
+        const parsed = z
           .object({ email: z.email(), password: z.string().min(6) })
           .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (!parsed.success) return null;
 
-          if (passwordsMatch) return user;
-        }
+        const { email, password } = parsed.data;
+        const user = await getUser(email);
+        if (!user) return null;
 
-        console.log("Invalid credentials");
-        return null;
+        const ok = await bcrypt.compare(password, user.password);
+        return ok ? user : null;
       },
     }),
   ],
+  // создание пользователя в таблице users
+  events: {
+    async signIn({ user, account, profile }) {
+      if (account?.provider !== "google") return;
+
+      const email = user?.email;
+      const name = user?.name || (profile as any)?.name || "GoogleUser";
+      if (!email) return;
+
+      try {
+        await sql`
+        INSERT INTO users (name, email, password)
+        VALUES (${name}, ${email}, ${null})
+        ON CONFLICT (email) DO NOTHING
+      `;
+      } catch (e) {
+        console.error("Upsert Google user failed:", e);
+      }
+    },
+  },
 });
