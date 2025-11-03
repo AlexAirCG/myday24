013e3167f3da5ec628196835af350cae80aee86e
AlexAirCG <alexairggg@yandex.ru>
2025-11-03 19:03:47 +0300

    19:03 fix data.ts

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,3 @@
-// import { fetchUserEmail } from "../lib/data";
-// import SideNav from "../ui/dashboard/sidenav";
-
-// export default async function Layout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const email = await fetchUserEmail();
-//   return (
-//     <div className="flex h-[100svh] flex-col md:flex-row overflow-hidden">
-//       <div className="flex-none w-full md:w-64">
-//         <SideNav email={email} />
-//       </div>
-//       <div className="flex-1 min-h-0 bg-sky-200 p-3 md:p-12 overflow-hidden">
-//         {children}
-//       </div>
-//     </div>
-//   );
-// }
 import { fetchUserEmail } from "../lib/data";
 import SideNav from "../ui/dashboard/sidenav";
 
